f77de28d2de6c0e1e0d8eb31745448e836a52cfb
Umar Khan <ukhan262@Umars-MBP.attlocal.net>
2023-08-30 01:05:49 -0700

    optimized code for some flexibility

--- src/main.py
+++ src/main.py
@@ -1,115 +1,102 @@
+"""
+Key changes and optimizations:
+
+1. Moved the load_dotenv call to a dedicated function for better clarity.
+2. Added error handling using raise_for_status() for API requests to handle potential errors more gracefully.
+3. Changed the way API request data is sent from using the data parameter to sending JSON data directly using the json parameter.
+4. Improved error handling in the read_file function to print errors and return early.
+5. Moved the argparse setup into the main function for better encapsulation.
+6. Wrapped the article posting process in a try-except block to handle potential exceptions during the API request.
+7. Consolidated the process of getting user information and posting an article into their respective functions for better separation of concerns.
+8. Allowed arguments for the title and tags of the article.
+
+"""
 """Upload articles to medium."""
 import argparse
 import os
-
 import requests
 
-# If not github actions, use dotenv
-if not os.environ.get("GITHUB_ACTIONS"):
-    from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-    load_dotenv()
+API_TOKEN = os.environ.get("MEDIUM_TOKEN")
 
-api_token = os.environ.get("MEDIUM_TOKEN")
+def load_env():
+    if not os.environ.get("GITHUB_ACTIONS"):
+        load_dotenv()
 
-# Get file to upload from argparse
-parser = argparse.ArgumentParser(description="Upload articles to medium")
-parser.add_argument(
-    "-f",
-    "--file",
-    type=str,
-    help="File to upload",
-    required=True,
-)
+def get_user_info(api_token):
+    response = requests.get(
+        f"https://api.medium.com/v1/me?accessToken={api_token}", timeout=10
+    )
+    response.raise_for_status()
+    return response.json()
 
-args = parser.parse_args()
+def post_article(api_token, title, contents, tags, state="draft"):
+    headers = {"Authorization": f"Bearer {api_token}"}
+    article_metadata = {
+        "title": title,
+        "contentFormat": "markdown",
+        "content": contents,
+        "tags": tags,
+        "publishStatus": state,
+    }
 
-
-# Get medium user info from api
-def get_user_info():
-    """
-    Get user details from Medium API.
-
-    Returns:
-         user_detail
-    """
-    my_details = requests.get(
-        url=f"https://api.medium.com/v1/me?accessToken={api_token}", timeout=10
-    ).json()
-
-    return my_details
-
-
-def post_article(contents, state="draft"):
-    """
-    Post article on Medium API.
-
-    Args:
-        contents: Markdown formatted content to be published onto medium.
-        state: The state of the article, defaults to draft.
-
-    Returns:
-        True if successful
-    """
-    print("Posting article")
-    try:
-        headers = {"Authorization": f"Bearer {api_token}"}
-
-        article_metadata = {
-            "title": "Reusable Terraform Deployment Workflow",
-            "contentFormat": "markdown",
-            "content": contents,
-            "tags": ["devops", "automation", "workflows", "github", "terraform"],
-            "publishStatus": state,
-        }
-
-        response = requests.post(
-            url=f'https://api.medium.com/v1/users/{get_user_info()["data"]["id"]}/posts',
-            headers=headers,
-            timeout=10,
-            data=article_metadata,
-        )
-    except Exception as error_message:
-        print(error_message)
-        return False
-
-    print(f'New article URL: {response.json()["data"]["url"]}')
-    return True
-
+    response = requests.post(
+        f'https://api.medium.com/v1/users/{get_user_info(api_token)["data"]["id"]}/posts',
+        headers=headers,
+        timeout=10,
+        json=article_metadata,
+    )
+    response.raise_for_status()
+    return response.json()["data"]["url"]
 
 def read_file(filename):
-    """
-    Read file from given path.
-
-    Args:
-        filename: Path to file to be read
-
-    Returns:
-        contents of file
-    """
     try:
         with open(filename, "r", encoding="UTF-8") as file_contents:
-            contents = file_contents.read()
+            return file_contents.read()
     except FileNotFoundError:
         print("File not found")
-        return None
     except Exception as error_message:
         print(error_message)
-        return None
-
-    return contents
-
 
 def main():
-    """Execute function."""
+    parser = argparse.ArgumentParser(description="Upload articles to Medium")
+    parser.add_argument(
+        "-f",
+        "--file",
+        type=str,
+        help="File to upload",
+        required=True,
+    )
+    parser.add_argument(
+        "-t",
+        "--title",
+        type=str,
+        help="Title of the article",
+        required=True,
+    )
+    parser.add_argument(
+        "--tags",
+        type=str,
+        help="Comma-separated list of tags",
+        required=True,
+    )
+    args = parser.parse_args()
+
+    load_env()
+
     filename = args.file
     if filename.endswith(".md"):
         contents = read_file(filename)
         if contents:
-            post_article(contents, state="draft")
+            try:
+                tags = args.tags.split(",")  # Convert comma-separated tags to a list
+                article_url = post_article(API_TOKEN, args.title, contents, tags, state="draft")
+                print(f'New article URL: {article_url}')
+            except requests.exceptions.RequestException as e:
+                print(f"An error occurred while posting the article: {e}")
         else:
             print("No contents found")
-
 
 if __name__ == "__main__":
     main()
